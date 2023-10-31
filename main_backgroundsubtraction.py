a4b8beb17d0dd6bdcdcf724390fbffb19444639d
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-11-01 01:00:11 +0900

    alpha=0.8, 47% & 88%

--- main_backgroundsubtraction.py
+++ main_backgroundsubtraction.py
@@ -9,64 +9,73 @@ import evaluation as eval
 
 def main():
 
-    ##### Set threshold
-    threshold = 20
-    
-    ##### Set path
-    input_path = './input_image'    # input path
-    gt_path = './groundtruth'       # groundtruth path
-    result_path = './result'        # result path
+		##### Set threshold
+		threshold = 20
 
-    ##### load input
-    input = [img for img in sorted(os.listdir(input_path)) if img.endswith(".jpg")]
+		##### Set path
+		input_path = './input_image'    # input path
+		gt_path = './groundtruth'       # groundtruth path
+		result_path = './result'        # result path
 
-    ##### first frame and first background
-    frame_current = cv.imread(os.path.join(input_path, input[0]))
-    frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
-    frame_prev_gray = frame_current_gray
+		##### load input
+		input = [img for img in sorted(os.listdir(input_path)) if img.endswith(".jpg")]
 
+		##### first frame and first background
+		frame_current = cv.imread(os.path.join(input_path, input[0]))
+		frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+		frame_prev_gray = frame_current_gray
 
-    ##### background substraction
-    for image_idx in range(len(input)):
+		##### Set learning rate of the background
+		alpha=0.8
+		# small alpha 1 to 0.0001
+		background_model_prev=frame_prev_gray
 
-        ##### calculate foreground region
-        diff = frame_current_gray - frame_prev_gray
-        diff_abs = np.abs(diff).astype(np.float64)
+		##### background substraction
+		for image_idx in range(len(input)):
 
-        ##### make mask by applying threshold
-        frame_diff = np.where(diff_abs > threshold, 1.0, 0.0)
+			##### Adaptive B/F Detection
+			background_model_current=(1-alpha)*background_model_prev+alpha*frame_prev_gray
 
-        ##### apply mask to current frame
-        current_gray_masked = np.multiply(frame_current_gray, frame_diff)
-        current_gray_masked_mk2 = np.where(current_gray_masked > 0, 255.0, 0.0)
+			##### calculate foreground region
+			diff = frame_current_gray-background_model_current
+			diff_abs = np.abs(diff).astype(np.float64)
 
-        ##### final result
-        result = current_gray_masked_mk2.astype(np.uint8)
-        #cv.imshow('result', result) # colab does not support cv.imshow
+			##### make mask by applying threshold
+			frame_diff = np.where(diff_abs > threshold, 1.0, 0.0)
 
-        ##### renew background
-        frame_prev_gray = frame_current_gray
+			##### apply mask to current frame
+			current_gray_masked = np.multiply(frame_current_gray, frame_diff)
+			current_gray_masked_mk2 = np.where(current_gray_masked > 0, 255.0, 0.0)
 
-        ##### make result file
-        ##### Please don't modify path
-        cv.imwrite(os.path.join(result_path, 'result%06d.png' % (image_idx + 1)), result)
+			##### final result
+			result = current_gray_masked_mk2.astype(np.uint8)
+			#cv.imshow('result', result) # colab does not support cv.imshow
 
-        ##### end of input
-        if image_idx == len(input) - 1:
-            break
+			##### renew background
+			frame_prev_gray = frame_current_gray
+			background_model_prev=background_model_current
 
-        ##### read next frame
-        frame_current = cv.imread(os.path.join(input_path, input[image_idx + 1]))
-        frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+			##### make result file
+			##### Please don't modify path
+			cv.imwrite(os.path.join(result_path, 'result%06d.png' % (image_idx + 1)), result)
 
-        ##### If you want to stop, press ESC key
-        k = cv.waitKey(30) & 0xff
-        if k == 27:
-            break
+			##### end of input
+			if image_idx == len(input) - 1:
+				break
 
-    ##### evaluation result
-    eval.cal_result(gt_path, result_path)
+			##### read next frame
+			frame_current = cv.imread(os.path.join(input_path, input[image_idx + 1]))
+			frame_current_gray = cv.cvtColor(frame_current, cv.COLOR_BGR2GRAY).astype(np.float64)
+
+			##### If you want to stop, press ESC key
+			k = cv.waitKey(30) & 0xff
+			if k == 27:
+				break
+
+		##### evaluation result
+		eval.cal_result(gt_path, result_path)
+
 
 if __name__ == '__main__':
-    main()
+	main()
 
